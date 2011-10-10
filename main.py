30b9e1bb18769fc8d8bbf30bd615af535c46074f
Joey Rhyu <jrhyu@jrhyu-mac.local>
2011-10-09 22:20:53 -0400

    fb app fix

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ from django.utils import simplejson as json
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
+
 
 FB_NONDEV_ID = '210687712331095'
 FB_NONDEV_SECRETID = '2d92ebb40913357c449332f172b1366a'
@@ -46,7 +48,7 @@ FB_DEV_SECRETID = '9dc16baa1f8dc2aa1710491019a49c6f'
 
 FB_APP_ID = '262869930403008'
 FB_SECRET_ID = '9dc16baa1f8dc2aa1710491019a49c6f'
-_USER_FIELDS_API = u'name,email,picture,friends,first_name,last_name,statuses'
+_USER_FIELDS_API = u'name,email,picture,friends,first_name,last_name,statuses,checkins'
 
 """data models"""
 class Users(db.Model):
@@ -59,13 +61,19 @@ class Users(db.Model):
     email = db.StringProperty()
     friends = db.StringListProperty()
     checkin = db.StringListProperty()
+    status = db.StringListProperty()
     city = db.StringProperty()
+
+class List(db.Model):
+    user_id = db.StringProperty(required = True)
+    list_topic = db.StringProperty(required = True)
+    list_item = db.StringProperty()
 """end data models"""
 
 class Facebook(object):
     def __init__(self, app_id = FB_APP_ID, 
              app_secret = FB_SECRET_ID):
-        logging.debug('Facebook class - init')           
+        #logging.debug('Facebook class - init')           
         self.app_id = app_id
         self.app_secret = app_secret
         self.user_id = None
@@ -76,39 +84,39 @@ class Facebook(object):
     def load_signed_request(self, signed_request):
 	    #load the user state; signed_request is used to share info between fb adn the app
         try: 
-            logging.debug('Facebook class - load_signed_request - try')
-            logging.debug(signed_request)
+            #logging.debug('Facebook class - load_signed_request - try')
+            #logging.debug(signed_request)
             sig, load = signed_request.split(u'.', 1)
-            logging.debug('sig: ' + sig)
-            logging.debug('load: ' +load)
+            #logging.debug('sig: ' + sig)
+            #logging.debug('load: ' +load)
             sig = self.base64_url_decode(sig)
-            logging.debug('sig after base: ')
-            logging.debug(sig)
+            #logging.debug('sig after base: ')
+            #logging.debug(sig)
             deload = self.base64_url_decode(load)
-            logging.debug('deload after base')
-            logging.debug(deload)
+            #logging.debug('deload after base')
+            #logging.debug(deload)
             data = json.loads(deload)
             #data = json.loads(self.base64_url_decode(load))
-            logging.debug('data after json base: ')
-            logging.debug(data)
+            #logging.debug('data after json base: ')
+            ##logging.debug(data)
 		    
 		    #check the signature
       	    expected_sig = hmac.new(
 		        self.app_secret, msg = load, digestmod = hashlib.sha256).digest()
-            logging.debug('expected_sig: ')
-            logging.debug(expected_sig)
+            #logging.debug('expected_sig: ')
+            #logging.debug(expected_sig)
 
 		    #allow request to function for 1 day
     	    if sig == expected_sig and data[u'issued_at'] > (time.time() - 86400):
-                logging.debug('sig == expected sig')
+                #logging.debug('sig == expected sig')
                 self.signed_request = data
                 self.user_id = data.get(u'user_id')
-                logging.debug(self.user_id)
+                #logging.debug(self.user_id)
                 self.access_token = data.get(u'oauth_token')
-                #logging.debug(self.access_token + 'access token') 
+                ##logging.debug(self.access_token + 'access token') 
 		
         except ValueError, ex:
-            logging.debug('except')
+            #logging.debug('except')
             pass  
     
     @staticmethod
@@ -120,26 +128,26 @@ class Facebook(object):
     #me = facebook.api(u'/me', {u'fields': _USER_FIELDS})
     def api(self, path, params=None, method=u'GET', domain=u'graph'):
         """Make Graph API calls"""
-        logging.debug('in API')
-        logging.debug(path)
+        #logging.debug('in API')
+        #logging.debug(path)
         if not params:
             params = {}
         params[u'method'] = method
         if u'access_token' not in params and self.access_token:
-            logging.debug('API access token not in params')
+            #logging.debug('API access token not in params')
             params[u'access_token'] = self.access_token
-        logging.debug(params)
+        #logging.debug(params)
         url_f = urlfetch.fetch(
             url = u'https://' + domain + u'.facebook.com' + path,
             payload = urllib.urlencode(params),
             method = urlfetch.POST,
             headers = {
                 u'Content-Type': u'application/x-www-form-urlencoded'})
-        logging.debug('url_f')
-        logging.debug(url_f)
+        ##logging.debug('url_f')
+        ##logging.debug(url_f)
         result = json.loads(url_f.content) 
-        logging.debug('result')
-        logging.debug(result)
+        ##logging.debug('result')
+        ##logging.debug(result)
         #need to add instance API error
         return result
 
@@ -148,31 +156,31 @@ class BaseHandler(webapp.RequestHandler):
     user = None
     
     def initialize(self, request, response):
-        logging.debug('BaseHandler Initialize')
+        #logging.debug('BaseHandler Initialize')
         super(BaseHandler, self).initialize(request, response)
-        logging.debug(request)
-        logging.debug(response)
+        #logging.debug(request)
+        #logging.debug(response)
 
         try: 
-            logging.debug('BaseHandler Try - init_facebook()')           
+            #logging.debug('BaseHandler Try - init_facebook()')           
             self.init_facebook()
         except Exception, ex: 
             #self.log_exception(ex)
             raise
 
     def init_facebook(self):
-        logging.debug('BaseHandler - init_facebook()')           
+        #logging.debug('BaseHandler - init_facebook()')           
         facebook = Facebook()
         user = None
 
         # the initial facebook request is POST and not GET
         if u'signed_request' in self.request.POST:
-            logging.debug('BaseHandler init_facebook() - if signed request check')           
+            #logging.debug('BaseHandler init_facebook() - if signed request check')           
             """call load_signed_request to check on fb signature"""
-            logging.debug('SR in init_fb: ' + self.request.get('signed_request'))
+            #logging.debug('SR in init_fb: ' + self.request.get('signed_request'))
             facebook.load_signed_request(self.request.get('signed_request'))
-            logging.debug('after fb load_signed_request: ')
-            logging.debug(facebook.user_id)
+            #logging.debug('after fb load_signed_request: ')
+            #logging.debug(facebook.user_id)
             """we also want to change the POST request from fb to GET"""
             self.request.method = u'GET'
 #            self.set_cookie(
@@ -180,13 +188,18 @@ class BaseHandler(webapp.RequestHandler):
 
         # load a user object
         if facebook.user_id:
-            logging.debug('user id exists for signed request')
-            logging.debug('call the API method test')
-            #me = facebook.api(u'/me', {u'fields': _USER_FIELDS_API})
+            #logging.debug('user id exists for signed request')
+            #logging.debug('call the API method test')
+            me = facebook.api(u'/me', {u'fields': _USER_FIELDS_API})
+            foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
+            #logging.debug('4sq')
+            #logging.debug(foursquare)
             user = Users.get_by_key_name(facebook.user_id)
+            logging.debug(user)
             if user:
                 logging.debug('user found in datastore')
                 me = facebook.api(u'/me',{u'fields': _USER_FIELDS_API})
+                self.me = me
                 logging.debug(me)
                 # update access token
                 if facebook.access_token != user.access_token:
@@ -201,15 +214,30 @@ class BaseHandler(webapp.RequestHandler):
                 logging.debug('call the API method')
                 logging.debug(str(facebook.user_id))
                 me = facebook.api(u'/me',{u'fields': _USER_FIELDS_API})
+                self.me = me
                 try:
-                    friends = [user[u'id'] for user in me[u'friends'][u'data']] 
+                    friendslist = [user[u'id'] for user in me[u'friends'][u'data']]  
+                    foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
+                    statuslist = [stat[u'message'] for stat in me[u'statuses'][u'data']]
+                    logging.debug('in try')
+                    logging.debug('print friends')
+                    logging.debug(friendslist)
+                    logging.debug('print 4sq')
+                    logging.debug(foursquare)
+                    logging.debug('status')
+                    logging.debug(statuslist)
                     logging.debug('try to load new user in datastore')
                     logging.debug(facebook.user_id + ' ' + facebook.access_token)
+                    logging.debug(me)
+                    logging.debug(me[u'first_name'])
+                    logging.debug(me[u'last_name'])
+                    logging.debug(me[u'name'])
                     user = Users(key_name = facebook.user_id, 
                         user_id = facebook.user_id,
                         access_token = facebook.access_token, name = me[u'name'], 
                         email = me[u'email'], picture = me[u'picture'],
-                        first_name = me[u'first_name'], last_name = me[u'last_name'])
+                        first_name = me[u'first_name'], last_name = me[u'last_name'],
+                        friends = friendslist) #, checkin = foursquare, status = statuslist)
                     user.put()
                     logging.debug('after put')
                 except: 
@@ -224,11 +252,16 @@ class MainHandler(BaseHandler):
         logging.debug('MainHandler Get')
         logging.debug(self.facebook.user_id)
         if self.facebook.user_id:
-            data = dict(current_user=self.user.user_id,
-                        picture = self.user.picture)
-            logging.debug(data)
+            #data = dict(current_user=self.user.user_id,
+                        #picture = self.user.picture)
+            #logging.debug(data)
             uid = self.facebook.user_id 
-            self.render(u'index', uid=uid)
+            fname = self.me[u'first_name']
+            picture = self.me[u'picture']
+            logging.debug(uid)
+            logging.debug(fname)
+            logging.debug(picture)
+            self.render(u'index', uid=uid, fname=fname, picture=picture)
         else:
             self.render(u'welcome')
 
