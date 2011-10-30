c13914ecd69c2a3722f79fc474395dd11975898d
Joey Rhyu <jrhyu@jrhyu-mac.home>
2011-10-30 01:50:35 -0400

    bunch of changes

--- main.py
+++ main.py
@@ -14,6 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+"""
+see all list actions
+curl 'https://graph.facebook.com/me/joeyrhyuappdev:listed?access_token=AAADvFCzS5MABABglXA3ZBZAtjWKETYe2ZAvQAvzWYXJS4SPIIJFuJoWNutOsuhlxpY8jenZC9qdCGll38lrWooN0ZCCq6kZA4ZD'
+
+create new list action
+curl -F 'access_token=AAADvFCzS5MABABglXA3ZBZAtjWKETYe2ZAvQAvzWYXJS4SPIIJFuJoWNutOsuhlxpY8jenZC9qdCGll38lrWooN0ZCCq6kZA4ZD' \
+     -F 'task=http://samples.ogp.me/263057930384208' \
+        'https://graph.facebook.com/me/joeyrhyuappdev:listed'
+
+delete list action
+curl -X DELETE \
+     -F 'access_token=AAADvFCzS5MABABglXA3ZBZAtjWKETYe2ZAvQAvzWYXJS4SPIIJFuJoWNutOsuhlxpY8jenZC9qdCGll38lrWooN0ZCCq6kZA4ZD' \
+        'https://graph.facebook.com/{'{id_from_create_call}'}'
+"""
+
 import logging
 import os
 import facebook 
@@ -38,7 +54,7 @@ from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 from google.appengine.api import urlfetch
-
+from google.appengine.ext.db import djangoforms
 
 FB_NONDEV_ID = '210687712331095'
 FB_NONDEV_SECRETID = '2d92ebb40913357c449332f172b1366a'
@@ -65,15 +81,16 @@ class Users(db.Model):
     city = db.StringProperty()
 
 class List(db.Model):
+    user_key = db.ReferenceProperty(Users)
     user_id = db.StringProperty(required = True)
     list_topic = db.StringProperty(required = True)
-    list_item = db.StringProperty()
+    list_item = db.StringListProperty()
 """end data models"""
 
 class Facebook(object):
     def __init__(self, app_id = FB_APP_ID, 
              app_secret = FB_SECRET_ID):
-        #logging.debug('Facebook class - init')           
+        logging.debug('Facebook class - init')           
         self.app_id = app_id
         self.app_secret = app_secret
         self.user_id = None
@@ -84,8 +101,8 @@ class Facebook(object):
     def load_signed_request(self, signed_request):
 	    #load the user state; signed_request is used to share info between fb adn the app
         try: 
-            #logging.debug('Facebook class - load_signed_request - try')
-            #logging.debug(signed_request)
+            logging.debug('Facebook class - load_signed_request - try')
+            logging.debug(signed_request)
             sig, load = signed_request.split(u'.', 1)
             #logging.debug('sig: ' + sig)
             #logging.debug('load: ' +load)
@@ -170,6 +187,7 @@ class BaseHandler(webapp.RequestHandler):
 
     def init_facebook(self):
         #logging.debug('BaseHandler - init_facebook()')           
+        logging.debug('in init_facebook')
         facebook = Facebook()
         user = None
 
@@ -191,16 +209,16 @@ class BaseHandler(webapp.RequestHandler):
             #logging.debug('user id exists for signed request')
             #logging.debug('call the API method test')
             me = facebook.api(u'/me', {u'fields': _USER_FIELDS_API})
-            foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
+            #foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
             #logging.debug('4sq')
             #logging.debug(foursquare)
             user = Users.get_by_key_name(facebook.user_id)
-            logging.debug(user)
+            #logging.debug(user)
             if user:
                 logging.debug('user found in datastore')
                 me = facebook.api(u'/me',{u'fields': _USER_FIELDS_API})
                 self.me = me
-                logging.debug(me)
+                #logging.debug(me)
                 # update access token
                 if facebook.access_token != user.access_token:
                     user.access_token = facebook.access_token
@@ -212,20 +230,20 @@ class BaseHandler(webapp.RequestHandler):
             if not user and facebook.access_token:
                 logging.debug('user does not exist in datastore')
                 logging.debug('call the API method')
-                logging.debug(str(facebook.user_id))
+                #logging.debug(str(facebook.user_id))
                 me = facebook.api(u'/me',{u'fields': _USER_FIELDS_API})
                 self.me = me
                 try:
                     friendslist = [user[u'id'] for user in me[u'friends'][u'data']]  
-                    foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
-                    statuslist = [stat[u'message'] for stat in me[u'statuses'][u'data']]
+                    #foursquare = [loc[u'place'] for loc in me[u'checkins'][u'data']]
+                    #statuslist = [stat[u'message'] for stat in me[u'statuses'][u'data']]
                     logging.debug('in try')
                     logging.debug('print friends')
-                    logging.debug(friendslist)
+                    #logging.debug(friendslist)
                     logging.debug('print 4sq')
-                    logging.debug(foursquare)
+                    #logging.debug(foursquare)
                     logging.debug('status')
-                    logging.debug(statuslist)
+                    #logging.debug(statuslist)
                     logging.debug('try to load new user in datastore')
                     logging.debug(facebook.user_id + ' ' + facebook.access_token)
                     logging.debug(me)
@@ -246,6 +264,53 @@ class BaseHandler(webapp.RequestHandler):
         self.facebook = facebook
         self.user = user 
 
+class ListHandler(webapp.RequestHandler):
+
+    def post(self):
+        listtype = self.request.get('listtype')
+        listvalue = self.request.get_all('listvalue')
+        user = Users.get(self.request.get('pkey'))
+        pkey = user.key()
+        uid = user.user_id
+        # if the list is empty, return an error
+#        if not listtype or listvalue:
+#            self.render(u'index', err = 'Add Values', picture=user.picture)
+#        else:
+        lists = List(user_key=pkey, user_id=uid, list_topic=listtype, list_item=listvalue)
+        lists.put()
+
+#        if user:
+#            logging.debug('true')
+#        else:
+#            logging.debug('false')
+#        listentry = List(user_key=pkey, user_id=uid, list_topic=listtype, list_item=listvalue)
+#        listentry.put()
+
+class ListDisplayHandler(webapp.RequestHandler):
+    def get(self):
+        logging.debug(self.request.get('pkey'))
+#        user = Users.all() #(self.request.get('pkey'))
+#        uid = user.user_id
+#        fname = user.first_name
+#        picture = user.picture
+        lists = List.all()
+#        logging.debug(lists)
+        render(self, u'lists', ulists=lists)
+
+    def post(self):
+        logging.debug(self.request.get('pkey'))
+        user = Users.get(self.request.get('pkey'))
+        logging.debug('listdisplayhandler Post')
+        uid = user.user_id
+        fname = user.first_name
+        picture = user.picture
+        lists = List.all()
+        logging.debug(user.list_set)
+        render(self, u'lists', uid=uid, fname=fname, picture=picture, ulists=user.list_set)
+        for i in user.list_set:
+            logging.debug(i.list_item)
+            logging.debug(i.list_topic)
+
 class MainHandler(BaseHandler):    
 
     def get(self):
@@ -261,20 +326,32 @@ class MainHandler(BaseHandler):
             logging.debug(uid)
             logging.debug(fname)
             logging.debug(picture)
-            self.render(u'index', uid=uid, fname=fname, picture=picture)
+            pkey = self.user.key()
+            render(self, u'index', uid=uid, fname=fname, picture=picture, pkey=pkey)
         else:
-            self.render(u'welcome')
+            render(self, u'welcome')
 
-    def render(self, name, **data):
-        logging.debug('in render')
-        if data:
-            logging.debug('data exists')
-        path = os.path.join(os.path.dirname(__file__), 'views', name + '.html') 
-        self.response.out.write(template.render(path, data))        
+    #def render(self, name, **data):
+    #    logging.debug('in render')
+    #    if data:
+    #        logging.debug('data exists')
+    #    path = os.path.join(os.path.dirname(__file__), 'views', name + '.html') 
+    #    self.response.out.write(template.render(path, data))        
+
+def render(self, name, **data):
+    logging.debug('in render')
+    logging.debug(data)
+    if data:
+        logging.debug('data exists')
+    path = os.path.join(os.path.dirname(__file__), 'views', name + '.html') 
+    self.response.out.write(template.render(path, data))
 
 def main():
 	logging.getLogger().setLevel(logging.DEBUG)
-	routes = [('/', MainHandler)]
+	logging.debug('main')
+	routes = [('/', MainHandler),
+	          ('/list', ListHandler),
+              ('/listdisplay', ListDisplayHandler)]
 	application = webapp.WSGIApplication(routes, debug=True)
 	util.run_wsgi_app(application)
 
